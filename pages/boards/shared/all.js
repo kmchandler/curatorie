79f5b205e346b23d792fc904c2fab05e0606a29c
Kristen Chandler <krismchandler@gmail.com>
2023-03-14 19:00:50 -0500

    styling cards

--- pages/boards/shared/all.js
+++ pages/boards/shared/all.js
@@ -38,7 +38,7 @@ function SharedBoards() {
           <button type="button" className="requestsButton">requests</button>
         </Link>
       </div>
-      <div className="d-flex flex-wrap cardContainer boardCardDiv sharedBoardCardsDiv">
+      <div className="d-flex flex-wrap cardContainer sharedBoardCardsDiv">
         {sharedBoards.map((sb) => <SharedBoardCard key={sb.id} boardObj={sb} onUpdate={getBoards} />)}
       </div>
     </div>
